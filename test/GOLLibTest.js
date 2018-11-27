69326e71c4698a338d43b03e34d0b5647e3fef97
praathul <44019979+athulpram@users.noreply.github.com>
2018-11-27 15:39:02 +0530

    Add test for invalid neighbour positions

--- test/GOLLibTest.js
+++ test/GOLLibTest.js
@@ -3,8 +3,13 @@ const {deepEqual} = require('assert');
 
 describe('Neighbour states',function(){
   universe.lifeExistance = [[0,0,0],[0,0,0],[0,0,0]];
-  it("should return an array filled with zero",function(){
+  it("should return an array filled with zero when all neighbours are valid",function(){
     deepEqual(universe.fetchNeighbourStates(1,1),[0,0,0,0,0,0,0,0]);   
+  });
+  it("should return zero for all invalid neighbour positions",function(){
     deepEqual(universe.fetchNeighbourStates(0,0),[0,0,0,0,0,0,0,0]);   
+    deepEqual(universe.fetchNeighbourStates(0,1),[0,0,0,0,0,0,0,0]);   
+    deepEqual(universe.fetchNeighbourStates(0,2),[0,0,0,0,0,0,0,0]);   
+    deepEqual(universe.fetchNeighbourStates(1,0),[0,0,0,0,0,0,0,0]);   
   });
 });
