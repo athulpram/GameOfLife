ac030b4591d46e730aa45ab0f24d4158b4be0560
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-11-27 22:47:37 +0530

    modified question's message in main file

--- gameOfLife.js
+++ gameOfLife.js
@@ -2,7 +2,7 @@ const {world} = require('./src/GOLLib.js');
 const readline = require('readline-sync');
 
 const main = function() {
-  let iteration = readline.question('enter desired iteration value');
+  let iteration = readline.question('enter desired iteration value : ');
   console.log(world.runWorld(iteration));
 }
 
